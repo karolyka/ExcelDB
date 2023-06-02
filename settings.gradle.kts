6ab07218806fb4cc0cdc8b1395e9308fd34e32d3
Karoly Machalik <karoly_machalik@epam.com>
2023-06-02 08:13:30 +0200

    Publish to maven (locally) implemented

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,13 @@ pluginManagement {
     val detektVersion: String by settings
     val kotlinVersion: String by settings
     val ktlintVersion: String by settings
+    val dokkaVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion apply false
         id("org.jlleitschuh.gradle.ktlint") version ktlintVersion apply false
         id("io.gitlab.arturbosch.detekt") version detektVersion apply false
+        id("org.jetbrains.dokka") version dokkaVersion apply false
     }
 
     repositories {
